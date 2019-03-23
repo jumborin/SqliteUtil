ad866b906a9e42237b57738c86e268cfe1335e32
jumborin <y.f.architect@gmail.com>
2019-03-23 18:30:04 +0900

    ファイル削除用バッチを作成

---
+++ DELETE.bat
@@ -0,0 +1,6 @@
+
+REM アップロードに不要なデータを削除する。
+del /Q data\*.txt
+del /Q *.db
+del /Q *.sqlite3
+del /Q *.bak
--- sql/insert_data.sql
+++ sql/insert_data.sql
@@ -1,3 +1,4 @@
 
 /* テーブルにデータを登録する。 */
 insert into test values(1, 'TEST');
+insert into test values(2, 'TEST2');
