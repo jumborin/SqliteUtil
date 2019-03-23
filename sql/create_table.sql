7c9e295a8e50d1995922b83beac9db765d99c20d
jumborin <y.f.architect@gmail.com>
2019-03-23 20:53:46 +0900

    テーブル情報を修正

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,3 +1,8 @@
 
 /* テーブルを作成する。 */
-create table test(id,test);
+create table user(id text,name text,age integer,type text);
+create index user_idx on test(id);
+
+/* テーブルを作成する。 */
+create table history(exec_time text,userid text,action text,message text);
+create index history_idx on test(userid,action);
