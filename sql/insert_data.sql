69473c9a1a2501eb01f3d6a53560b5e84aa98f97
jumborin <y.f.architect@gmail.com>
2019-03-24 16:45:29 +0900

    insert用SQLを高速化のため、トランザクションを実装

--- sql/insert_data.sql
+++ sql/insert_data.sql
@@ -1,4 +1,12 @@
 
+/* トランザクション開始(処理高速化のため) */
+BEGIN;
+
 /* テーブルにデータを登録する。 */
-insert into test values('1', 'TEST');
+insert into test values('1', 'TEST1');
 insert into test values('2', 'TEST2');
+insert into test values('3', 'TEST3');
+
+
+/*コミット処理*/
+COMMIT;
