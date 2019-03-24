8cdd50dd0eaa130748b61000f9f1d63d8ed164e0
jumborin <y.f.architect@gmail.com>
2019-03-24 22:42:22 +0900

    dump対象のDBファイルが存在しなかった場合にスキップする処理を追加およびコメント修正

--- DUMP_DB.bat
+++ DUMP_DB.bat
@@ -11,6 +11,9 @@ for /f "tokens=1,* delims==" %%a in (db_setting.ini) do (
     set %%a=%%b
 )
 
+REM もしDBファイルが存在しなかったら以降の処理はスキップする。
+if not exist %DB_FILE% exit
+
 REM DBファイルのダンプファイルを作成するSQLファイル
 SET DUMP_SQL_FILE=sql/dump_sqlite_data.sql
 
--- IMPORT_DB.bat
+++ IMPORT_DB.bat
@@ -14,8 +14,11 @@ for /f "tokens=1,* delims==" %%a in (db_setting.ini) do (
 REM ダンプファイルインポート用SQLファイル
 SET IMPORT_DUMP_SQL_FILE=sql/import_dump_file.sql
 
+
 REM 実行処理
 
+
+REM 既に同名のDBファイルが存在していた場合はバックアップする。
 IF EXIST %DB_FILE% move %DB_FILE% %DB_FILE%_%DATE:/=%_%TIME::=%.bak
 
 REM CreateTable用のSQLファイルを実行する。
