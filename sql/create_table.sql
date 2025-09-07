fa906293fcecb73a95c3a8301bf8a3487ab7cce2
Jumborin <jumborin@users.noreply.github.com>
2025-09-07 21:36:26 +0900

    SQLファイルのバグを修正

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -3,11 +3,11 @@ create table test(id text,message text);
 
 /* テーブルを作成する。 */
 create table user(id text,name text,age integer,type text);
-create index user_idx on test(id);
+create index user_idx on user(id);
 
 /* テーブルを作成する。 */
 create table history(exec_time text,userid text,action text,message text);
-create index history_idx on test(userid,action);
+create index history_idx on history(userid,action);
 
 /* コネクションを切断する。*/
 .quit
